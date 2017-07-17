8bfea3e1d0767c15dec8ea8bc86b2c4226ca9f89
sususweet <tyq9702@sina.com>
2017-07-17 21:48:17 +0800

    Fix System Clock Initializing.

--- main.c
+++ main.c
@@ -21,6 +21,19 @@ void initSystem(void){
                                               // Set FLL Div = fDCOCLK/2
     __bic_SR_register(SCG0);                  // Enable the FLL control loop
 
+    // Worst-case settling time for the DCO when the DCO range bits have been
+    // changed is n x 32 x 32 x f_MCLK / f_FLL_reference. See UCS chapter in 5xx
+    // UG for optimization.
+    // 32 x 32 x 8 MHz / 32,768 Hz = 250000 = MCLK cycles for DCO to settle
+    __delay_cycles(250000);
+
+    // Loop until XT1,XT2 & DCO fault flag is cleared
+    do{
+        UCSCTL7 &= ~(XT2OFFG + XT1LFOFFG + XT1HFOFFG + DCOFFG);
+        // Clear XT2,XT1,DCO fault flags
+        SFRIFG1 &= ~OFIFG;                      // Clear fault flags
+    }
+    while (SFRIFG1 & OFIFG);                   // Test oscillator fault flag
 }
 
 int main(void){
