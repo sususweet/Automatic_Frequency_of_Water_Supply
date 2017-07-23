eae6f340ad30908e1a435211c7f5eb916b206d54
Vera W <724152024@qq.com>
2017-07-23 20:10:26 +0800

    add UART settings
    finished UART test

---
+++ RS232.h
@@ -0,0 +1,12 @@
+//
+// Created by w on 2017/7/23.
+//
+
+#ifndef UART_TEST_RS232_H
+#define UART_TEST_RS232_H
+
+void Init_RSUART(void);
+
+void RS232TX_SEND(char type, unsigned char *tx_buf);
+
+#endif //UART_TEST_RS232_H
